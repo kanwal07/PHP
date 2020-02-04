452fc0ff9645d9979bca93270d12e9b038fb8c1c
kanwal07 <kanwalprit@gmail.com>
2020-02-04 00:40:46 -0500

    Still practicing 2 classes

---
+++ 2MoreClasses/Employee.class.php
@@ -0,0 +1,14 @@
+<?php
+
+class Employee
+{
+    private $empId;
+    private $name;
+    private $salary;
+    private static $sequence=0;
+    
+    function __construct(){
+        
+    }
+}
+
---
+++ 2MoreClasses/testEmployee.php
@@ -0,0 +1,14 @@
+<?php
+/*1. Create the following class 
+ * Employee - empID,name,salary
+ * constructor, setters/getters, toString()
+ * 
+ * Job- jobId, jobTitle
+ * constructor, getter/setter, toString()
+ * 
+ * 2. Create the TestEmployee class
+ * -create 5 job instances
+ * -create 3 employees
+ * -display all employees with corresponding jobs*/
+
+
